ff6c4110bc2b25e77117e4a1351ad8100a5e12fc
Vidula Deneth Salwathura <105682219+viduwaa@users.noreply.github.com>
2023-10-23 09:04:04 +0530

    Add files via upload

---
+++ Tower Defense/Scenes/MainScenes/HUD.gd
@@ -0,0 +1,5 @@
+extends Control
+
+
+func _on_PausePlay_pressed():
+	$PauseText.visible = !$PauseText.visible
---
+++ Tower Defense/Signletons/GameData.gd
@@ -0,0 +1,16 @@
+extends Node
+
+var tower_data = {
+	"GunT1":{
+		"damage" :20,
+		"rof":6,
+		"range":350},
+	"MissileT1":{
+		"damage" :60,
+		"rof":3,
+		"range":550},
+	"CanonT1":{
+		"damage" :40,
+		"rof":4,
+		"range":250}
+	}
